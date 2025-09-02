f695f5eb756ff7658e2bfd71314a73bf2310d42f
Armaan Singh <08armaan21singh@gmail.com>
2025-09-02 17:21:48 -0500

    oops why are these paths don't seem right

--- build_libgit2.sh
+++ build_libgit2.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+base_dir=$(pwd)
+
 cd libgit2
 
 mkdir build
 
 cd build
 
-emcmake cmake .. -DUSE_HTTPS=on -DOPENSSL_INCLUDE_DIR=/home/techgeeks/libgit2-wasm/openssl_files/include/openssl/ -DOPENSSL_SSL_LIBRARY=/home/techgeeks/libgit2-wasm/openssl_files/libssl.a -DOPENSSL_CRYPTO_LIBRARY=/home/techgeeks/libgit2-wasm/openssl_files/libcrypto.a
+emcmake cmake .. -DUSE_HTTPS=on -DOPENSSL_INCLUDE_DIR=${base_dir}/openssl_files/include/openssl/ -DOPENSSL_SSL_LIBRARY=${base_dir}/openssl_files/libssl.a -DOPENSSL_CRYPTO_LIBRARY=${base_dir}/openssl_files/libcrypto.a
 
 cmake --build .
