48a16dc36425aeb827a64b25df30bc3623a89d4b
Armaan Singh <08armaan21singh@gmail.com>
2025-09-06 22:18:08 -0500

    increase stack size

--- build_libgit2.sh
+++ build_libgit2.sh
@@ -8,6 +8,6 @@ mkdir build
 
 cd build
 
-emcmake cmake .. -DUSE_HTTPS=on -DOPENSSL_INCLUDE_DIR=${base_dir}/openssl_files/include/openssl/ -DOPENSSL_SSL_LIBRARY=${base_dir}/openssl_files/libssl.a -DOPENSSL_CRYPTO_LIBRARY=${base_dir}/openssl_files/libcrypto.a -DCMAKE_C_FLAGS="-s STACK_SIZE=131072"
+emcmake cmake .. -DUSE_HTTPS=on -DOPENSSL_INCLUDE_DIR=${base_dir}/openssl_files/include/openssl/ -DOPENSSL_SSL_LIBRARY=${base_dir}/openssl_files/libssl.a -DOPENSSL_CRYPTO_LIBRARY=${base_dir}/openssl_files/libcrypto.a -DCMAKE_C_FLAGS="-s STACK_SIZE=2197152"
 
 cmake --build .
