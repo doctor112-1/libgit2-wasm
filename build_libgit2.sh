527bc68ece58a1bc4de19d5fc1e39c1b9419816a
Armaan Singh <08armaan21singh@gmail.com>
2025-09-07 21:51:43 -0500

    build libgit2 with -j12

--- build_libgit2.sh
+++ build_libgit2.sh
@@ -10,4 +10,4 @@ cd build
 
 emcmake cmake .. -DUSE_HTTPS=on -DOPENSSL_INCLUDE_DIR=${base_dir}/openssl_files/include/openssl/ -DOPENSSL_SSL_LIBRARY=${base_dir}/openssl_files/libssl.a -DOPENSSL_CRYPTO_LIBRARY=${base_dir}/openssl_files/libcrypto.a -DCMAKE_C_FLAGS="-s STACK_SIZE=2197152"
 
-cmake --build .
+cmake --build . -j$(nproc)
